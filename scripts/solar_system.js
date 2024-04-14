881fdacf89e445bb1b7e194fb1e61044861c0404
n72019fu <felix.underwood@student.manchester.ac.uk>
2024-04-14 09:30:55 +0100

    basic solar system page

---
+++ scripts/solar_system.js
@@ -0,0 +1,10 @@
+fetch('http://localhost:3000/getPlanets/')
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
