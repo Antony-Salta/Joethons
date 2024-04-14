c57287a73814ab93dcd35b83aa6f0da33b0d0a6a
r23391as <antony.salta@student.manchester.ac.uk>
2024-04-14 08:28:59 +0100

    Made the explore program read from a json file instead

--- explore/explore.py
+++ explore/explore.py
@@ -1,84 +1,92 @@
 # Example file showing a circle moving on screen
 import pygame
 from sprites import Telescope, Hammer, Thermometer, Clock, Player
+import json
 # pygame setup
 
-planet = {
-    "Earth" : {"Name": "Earth", "featherSpeed" : 100, "hammerSpeed" : 400, "maxTemp" : 57.2, "minTemp": -89.2, "dayLength" : 24,
-               "skyText": """
-        The Earth has one moon, just called The Moon.
-        creative...
-        The moon can only be seen from light reflecting off the Sun at us.
-        As the Moon's position changes, the way it looks changes.
-        The main stages are the new moon, crescent moon,
-        half moon, gibbon moon and full moon, going from nearly invisible to fully visible
+# planets = {
+#     "Earth" : {"Name": "Earth", "featherSpeed" : 100, "hammerSpeed" : 400, "maxTemp" : 57.2, "minTemp": -89.2, "dayLength" : 24,
+#                "skyText": """
+#         The Earth has one moon, just called The Moon.
+#         creative...
+#         The moon can only be seen from light reflecting off the Sun at us.
+#         As the Moon's position changes, the way it looks changes.
+#         The main stages are the new moon, crescent moon,
+#         half moon, gibbon moon and full moon, going from nearly invisible to fully visible
          
-        There's also last of the sunset at the bottom, making different 
-        colours because of how theEarth's atmosphere scatters the light.""",
+#         There's also last of the sunset at the bottom, making different 
+#         colours because of how theEarth's atmosphere scatters the light.""",
         
-        "gravityText": """Hammer time!
-        Gravity accelerates everything at the same rate.
-        So with no atmosphere to slow things down, the hammer and feather
-        would fall at the same speed.
-        But because the hammer is denser than the feather, it's affected by 
-        air resistance less than the feather, so hits the ground earlier""",
-        "TemperatureText": """Nice and mild.
-        The highest temperature on Earth recordeed was 57.2C, 
-        and the lowest temperature recorder was -89.2C.
-        Compared to other planets, this is very mild, 
-        and part of why Earth is said to be in the 
-        "Goldilocks zone" of the solar system.
-        Not too hot, or too cold!""",
+#         "gravityText": """Hammer time!
+#         Gravity accelerates everything at the same rate.
+#         So with no atmosphere to slow things down, the hammer and feather
+#         would fall at the same speed.
+#         But because the hammer is denser than the feather, it's affected by 
+#         air resistance less than the feather, so hits the ground earlier""",
+#         "TemperatureText": """Nice and mild.
+#         The highest temperature on Earth recordeed was 57.2C, 
+#         and the lowest temperature recorder was -89.2C.
+#         Compared to other planets, this is very mild, 
+#         and part of why Earth is said to be in the 
+#         "Goldilocks zone" of the solar system.
+#         Not too hot, or too cold!""",
         
-        "temperatureText": """Nice and mild.
-        The highest temperature on Earth recordeed was 57.2°C, 
-        and the lowest temperature recorder was -89.2°C.
-        Compared to other planets, this is very mild, 
-        and part of why Earth is said to be in the 
-        "Goldilocks zone" of the solar system.
-        Not too hot, or too cold!""",
+#         "temperatureText": """Nice and mild.
+#         The highest temperature on Earth recordeed was 57.2°C, 
+#         and the lowest temperature recorder was -89.2°C.
+#         Compared to other planets, this is very mild, 
+#         and part of why Earth is said to be in the 
+#         "Goldilocks zone" of the solar system.
+#         Not too hot, or too cold!""",
         
-        "dayText": """Yeah, there's 24 hours in a day,
-        this is far more interesting for the other planets.
-        A day is the amount of time it takes for a planet to rotate
-        about its axis. And a year is the amount of time that it takes for a 
-        planet to rotate around the sun. A year is actually 365.24 days long.
-        That's why we have leap years,so that the calendar can catch up again."""},
-    "Mercury" : {"Name": "Mercury", "featherSpeed" : 133, "hammerSpeed" : 133, "maxTemp" : 420, "minTemp": -170, "dayLength": 176 * 24,
-                 "skyText" : """Ah. It's a bit big, innit?
-                 Mercury's too small to have anything around it, being less than 
-                 1.5x the size of our moon.
-                 You'll notice the sky is still black, even with the Sun out.
-                 That's because Mercury basically has no atmosphere, so there 
-                 is no air to scatter the light and make a sky.""",
+#         "dayText": """Yeah, there's 24 hours in a day,
+#         this is far more interesting for the other planets.
+#         A day is the amount of time it takes for a planet to rotate
+#         about its axis. And a year is the amount of time that it takes for a 
+#         planet to rotate around the sun. A year is actually 365.24 days long.
+#         That's why we have leap years,so that the calendar can catch up again."""},
+#     "Mercury" : {"Name": "Mercury", "featherSpeed" : 133, "hammerSpeed" : 133, "maxTemp" : 420, "minTemp": -170, "dayLength": 176 * 24,
+#                  "skyText" : """Ah. It's a bit big, innit?
+#                  Mercury's too small to have anything around it, being less than 
+#                  1.5x the size of our moon.
+#                  You'll notice the sky is still black, even with the Sun out.
+#                  That's because Mercury basically has no atmosphere, so there 
+#                  is no air to scatter the light and make a sky.""",
                  
-                 "gravityText": """Here, the gravity is a lot weaker at about 3.7 m/s^2
-                 so both of the objects fall slower. However, Mercury has almost no
-                 atmosphere, so there is no air resistance to slow down the feather.
-                 That means they fall together, and slowly.""",
+#                  "gravityText": """Here, the gravity is a lot weaker at about 3.7 m/s^2
+#                  so both of the objects fall slower. However, Mercury has almost no
+#                  atmosphere, so there is no air resistance to slow down the feather.
+#                  That means they fall together, and slowly.""",
                   
-                 "temperatureText": """Mercury's temperatures vary massively, because
-                 it's so close to the Sun, It can heat up 420°C during its long days.
-                 But because it has no atmosphere to hold heat in,
-                 the temperature can plummet to -170°C when its out of the sun.
-                 It's like an extreme version of the deserts on Earth.""",
+#                  "temperatureText": """Mercury's temperatures vary massively, because
+#                  it's so close to the Sun, It can heat up 420°C during its long days.
+#                  But because it has no atmosphere to hold heat in,
+#                  the temperature can plummet to -170°C when its out of the sun.
+#                  It's like an extreme version of the deserts on Earth.""",
                  
-                 "dayText" : """Mercury's days are extremely long. In fact, 
-                 they are longer than a year on Mercury!
-                 A Mercury day takes 176 Earth days to complete.
-                 A year on Mercury is only 88 days, so a day on Mercury takes
-                 nearly twice as long!
+#                  "dayText" : """Mercury's days are extremely long. In fact, 
+#                  they are longer than a year on Mercury!
+#                  A Mercury day takes 176 Earth days to complete.
+#                  A year on Mercury is only 88 days, so a day on Mercury takes
+#                  nearly twice as long!
                  
-                 Also, Mercury has a very elliptical orbit, meaning it orbits in
-                 an oval around the Sun.
-                 This means that when Mercury is close to the Sun, 
-                 it can actually stop or move slighly backward across the sky ,like a less 
-                 extreme version of noon going on for double the time here."""
-                 }
-}# this name thing is dumb, but i only pass the inner dictionary in to the interactables and I need the name there
+#                  Also, Mercury has a very elliptical orbit, meaning it orbits in
+#                  an oval around the Sun.
+#                  This means that when Mercury is close to the Sun, 
+#                  it can actually stop or move slighly backward across the sky ,like a less 
+#                  extreme version of noon going on for double the time here."""
+#                  }
+# }# this name thing is dumb, but i only pass the inner dictionary in to the interactables and I need the name there
+
+# with open("explore/planets.json", "w") as outfile:
+#     json.dump(planets,outfile)
+   
+with open("explore/planets.json", "r") as infile: 
+    planets = json.load(infile)
+
 
 #TODO, change this to read from a file or something
-chosenPlanet = planet["Mercury"]
+chosenPlanet = planets["Earth"]
 
 pygame.init()
 screen_size = (1280,720)
